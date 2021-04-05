e307760b0001057cb289f10f20e7c3b99f3617b8
JashanB <jashan_brar50@hotmail.com>
2021-04-05 13:02:43 -0400

    adding folder structure and dependencies

---
+++ src/components/app/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  //home page with create "room" button
+  //then whiteboard page with toolbar on left, no header? 
+  //path will be randomized string of 8 digits to make unique
+  //make share link button? would pop up with shareable link, copy button, and an x to close (click off too) - add with portal after 
+  //features: clear all, increase size of pen, z
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
