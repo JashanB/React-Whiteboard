97382e6b067b33ef254956f830cb397493dc1fe4
JashanB <jashan_brar50@hotmail.com>
2021-04-06 21:37:23 -0400

    cors fixed

--- server/index.js
+++ server/index.js
@@ -1,63 +1,63 @@
-// const express = require('express');
-// const app = express();
-// const socketIo = require("socket.io");
-
-// const http = require("http");
-// const port = process.env.PORT || 4001;
-// const index = require("../public/index");
-// const app = express();
-
-// app.use(index);
-// const server = http.createServer(app);
-// const io = socketIo(server);
-// // const server = http.createServer(app);
-// // const socket = require('socket.io');
-// // const io = require('socket.io')(http);
-// io.on('connection', onConnection);
-
-
-// // const port = 8080;
-// // server.listen(port, () => console.log(`server is running on port ${port}`));
-// http.listen(3001, () => {
-//   console.log('listening on *:3000');
-// });
-
 const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
+// const http = require("http");
+const httpServer = require("http").createServer();
+const cors = require('cors')
+const app = express();
+
+app.use(cors())
+
+// const socketIo = require("socket.io");
 
 const port = process.env.PORT || 4001;
 const index = require("./routes/index");
 
-const app = express();
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+// app.use(index);
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+// const server = http.createServer(app);
+
+// const io = socketIo(server);
+const io = require("socket.io")(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
 });
-const server = http.createServer(app);
 
-const io = socketIo(server);
+let interval;
 
-// let interval;
+// io.on("connection", (socket) => {
+//   console.log("New client connected");
 
+//   socket.on('drawing', getApiAndEmit(socket));
+//   socket.on("disconnect", () => {
+//     console.log("Client disconnected");
+//   });
+// });
 io.on("connection", (socket) => {
   console.log("New client connected");
-  // if (interval) {
-  //   clearInterval(interval);
-  // }
-  socket.on('drawing', getApiAndEmit(socket));
-  // interval = setInterval(() => getApiAndEmit(socket), 1000);
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
   socket.on("disconnect", () => {
     console.log("Client disconnected");
-    // clearInterval(interval);
+    clearInterval(interval);
   });
 });
+// const getApiAndEmit = socket => {
+//   console.log('triggered')
+//   // console.log({socket})
+//   // socket.on('drawing', (data) => socket.emit('drawing', data));
+// };
 
 const getApiAndEmit = socket => {
-  console.log('triggered')
-  // console.log({socket})
-  socket.on('drawing', (data) => socket.emit('drawing', data));
+  const response = new Date();
+  // Emitting a new message. Will be consumed by the client
+  socket.emit("FromAPI", response);
 };
-
-server.listen(port, () => console.log(`Listening on port ${port}`));
+httpServer.listen(port, () => console.log(`Listening on port ${port}`));
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,25 @@
 import './App.css';
 import Board from '../board';
+import React, { useState, useEffect } from "react";
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://localhost:4001/";
+const io = require("socket.io-client");
+
+// const socket = io(ENDPOINT, {
+//   withCredentials: true,
+//   extraHeaders: {
+//     "my-custom-header": "abcd"
+//   }
+// });
 
 function App() {
+  const [response, setResponse] = useState("");
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on("FromAPI", data => {
+      setResponse(data);
+    });
+  }, []);
   //home page with create "room" button
   //then whiteboard page with toolbar on left, no header? 
   //path will be randomized string of 8 digits to make unique
@@ -9,7 +27,10 @@ function App() {
   //features: clear all, increase size of pen, z
   return (
     <div className="App">
-      <Board />
+      {/* <Board /> */}
+      <p>
+      It's <time dateTime={response}>{response}</time>
+    </p>
     </div>
   );
 }
